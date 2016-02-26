599808cb891b87c08b17650cf3a8c831aa8a1eae
Elisa Huang <tingyih@uci.edu>
2016-02-25 18:17:46 -0800

    edit reg1 and regtb

--- reg1/reg1.vhd
+++ reg1/reg1.vhd
@@ -1,7 +1,7 @@
 -------------------------------------------------------------------
 -- Copyright MIPS_R_US 2016 - All Rights Reserved 
 --
--- File: buffer_2_woe.vhd
+-- File: reg1.vhd
 -- Team: MIPS_R_US
 -- Members:
 -- 		Stefan Cao (ID# 79267250)
@@ -10,7 +10,7 @@
 --		Linda Vang (ID# 71434490)
 --
 -- Description:
---		buffer with clk, 2 inputs, and 3outputs
+--		buffer with clk, 3 inputs, and 2 outputs
 --
 -- History:
 --     Date	    Update Description	            Developer
@@ -26,7 +26,8 @@ PORT (
 		ref_clk : IN std_logic;
 		DataI_A : IN std_logic_vector(31 DOWNTO 0);
 		DataI_B : IN std_logic_vector(31 DOWNTO 0);
-		clren:	  IN std_logic; --enable stallD
+		en:	  	  IN std_logic; --enable from PCSrc
+		clr:	  IN std_logic; --enable stallD
 		DataO_A : OUT std_logic_vector(31 DOWNTO 0);
 		DataO_B: OUT std_logic_vector(31 DOWNTO 0)
 		
@@ -34,13 +35,13 @@ PORT (
 end reg1;
 architecture behavior of reg1 is
 begin
-	process(ref_clk, DataI_A, DataI_B, clren)
+	process(ref_clk, DataI_A, DataI_B, clr,en)
 		variable temp_A : std_logic_vector(31 DOWNTO 0);
 		variable temp_B : std_logic_vector(31 DOWNTO 0);
 		
 	begin
 		if(ref_clk'event AND ref_clk='0') then
-			if(clren = '1') then
+			if(clr= '0' AND en = '1') then
 				temp_A := std_logic_vector(unsigned(DataI_A));
 				temp_B := std_logic_vector(unsigned(DataI_B));
 			end if;
--- reg1/reg1_tb.sv
+++ reg1/reg1_tb.sv
@@ -1,7 +1,7 @@
 /***************************************************************/
 // Copyright MIPS_R_US 2016 - All Rights Reserved 
 // 
-// File: memory_tb.sv
+// File: reg1_tb.sv
 // Team: MIPS_R_US
 // Members:
 //		Stefan Cao (ID# 79267250)
@@ -23,7 +23,8 @@ module reg1_tb;
 	logic ref_clk;
 	logic[31:0] DataI_A;
 	logic[31:0] DataI_B;
-	logic clren;
+	logic en;
+	logic clr;
 	logic[31:0] DataO_A;
 	logic[31:0] DataO_B;
   
@@ -31,7 +32,8 @@ reg1 L1(
           .ref_clk(ref_clk)
          ,.DataI_A(DataI_A)
          ,.DataI_B(DataI_B)
-		 ,.clren(clren)
+		 ,.en(en)
+		 ,.clr(clr)
          ,.DataO_A(DataO_A)
          ,.DataO_B(DataO_B)
          );
@@ -40,22 +42,32 @@ always #1 ref_clk = ~ ref_clk;
 
 initial begin
 	ref_clk = 1;
-	clren = 1;
+	clr = 0;
+	en = 1;
  	DataI_A = 32'b00000000000000000000011111111111;
  	DataI_B = 32'b00000000000000000000000000000000;
 	
 
  	#2
-	clren = 0; 
+	clr = 1; 
+	en = 1;
  	DataI_A = 32'b00000000000000000000000000000000;
  	DataI_B = 32'b00000000000000000000000000000000;
 	
  	#2 
-	clren = 1;
+	clr = 1;
+	en = 0;
  	DataI_A = 32'b00000000000000000000000000000000;
  	DataI_B= 32'b00000000000000000000000000000001;
 
- 	#2 DataI_A = 32'b00000000000000000000000000000010;
+ 	#2 
+	DataI_A = 32'b00000000000000000000000000000010;
+	
+	#2
+	clr = 0; 
+	en = 0;
+ 	DataI_A = 32'b00000000000000000000000000000000;
+ 	DataI_B = 32'b00000000000000000000000000000000;
 
 	$finish;
 
