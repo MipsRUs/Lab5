b8502a9c1a1511a3e28400d6e433a1485d98e12c
Stefan Cao <caostefan@gmail.com>
2016-02-27 23:29:26 -0800

    working on the processor

--- processor/processor.vhd
+++ processor/processor.vhd
@@ -87,6 +87,51 @@ component reg1
 	);
 end component;
 
+-- control ************************ NOT IMPLEMENTED NEED TO LOOK AT THIS AGAIN
+component control
+	PORT(
+		Op:				IN std_logic_vector(31 downto 26);
+		Funct:			IN std_logic_vector(5 downto 0);
+		RegWriteD:		OUT std_logic;
+		MemtoRegD:		OUT std_logic;
+		MemWriteD:		OUT std_logic;
+		ALUControlD:	OUT std_logic;
+		ALUSrcD: 		OUT std_logic;
+		RegDstD:		OUT std_logic;
+		BranchD:		OUT std_logic
+		
+	);
+end component;
+
+-- regfile
+component regfile
+	PORT (
+		ref_clk : IN std_logic;
+		we : IN std_logic ; -- write enable
+		raddr_1 : IN std_logic_vector (4 DOWNTO 0); -- read address 1
+		raddr_2 : IN std_logic_vector (4 DOWNTO 0); -- read address 2
+		waddr : IN std_logic_vector (4 DOWNTO 0); -- write address
+		rdata_1 : OUT std_logic_vector (31 DOWNTO 0); -- read data 1
+		rdata_2 : OUT std_logic_vector (31 DOWNTO 0); -- read data 2
+		wdata : IN std_logic_vector (31 DOWNTO 0) -- write data 1
+	);
+end component;
+
+-- sign_extension_16bit
+component sign_extension_16bit
+	PORT(
+		immediate : IN std_logic_vector(15 DOWNTO 0);
+		sign_extension_out : OUT std_logic_vector(31 DOWNTO 0)
+	);
+end component;
+
+-- shiftleft_32bit
+component shiftleft_32bit
+	PORT (
+		A_in : IN std_logic_vector (31 DOWNTO 0);
+		O_out: OUT std_logic_vector (31 DOWNTO 0)
+	);
+end component;
 
 
 
@@ -107,6 +152,29 @@ signal adder_value_4 : std_logic_vector (31 DOWNTO 0) := "0000000000000000000000
 
 signal InstrD : std_logic_vector(31 DOWNTO 0);
 
+signal RD1_out: std_logic_vector(31 DOWNTO 0);
+signal RD2_out: std_logic_vector(31 DOWNTO 0);
+
+signal ResultW: std_logic_vector(31 DOWNTO 0);
+
+signal SignImmD: std_logic_vector(31 DOWNTO 0);
+
+signal shift_out: std_logic_vector(31 DOWNTO 0);
+
+signal RegWriteD: std_logic;
+signal MemtoRegD: std_logic;
+signal MemWriteD: std_logic;
+signal ALUControlD: std_logic;
+signal ALUSrcD: std_logic;
+signal RegDstD: std_logic;
+signal BranchD: std_logic;
+
+
+signal RegWriteW: std_logic;
+signal WriteRegW: std_logic_vector(4 DOWNTO 0);
+
+
+
 signal StallF : std_logic;
 signal StallD : std_logic;
 
@@ -116,16 +184,28 @@ begin
 
 	PCmuxx:	mux PORT MAP(in0=>PCPlus4F, in1=>PC_adder_1, sel=>PCSrcD, outb=>PC_in);
 
-	PC_reg: buffer_e PORT MAP(ref_clk=>ref_clk, WE=>StallF, DataI=>PC_in, DataO=>PCF);
+	PC_regx: buffer_e PORT MAP(ref_clk=>ref_clk, WE=>StallF, DataI=>PC_in, DataO=>PCF);
 
-	IR: rom PORT MAP(addr=>PCF, dataOut=>IR_out);
+	IRx: rom PORT MAP(addr=>PCF, dataOut=>IR_out);
 
-	PCadder: adder PORT MAP(a=>PCF, b=>adder_value_4, sum=>PCPlus4F);
+	PCadderx: adder PORT MAP(a=>PCF, b=>adder_value_4, sum=>PCPlus4F);
 
-	Reg1: reg1 PORT MAP(ref_clk=>ref_clk, DataI=>IR_out, en=>stallD, clr=>PCSrcD,
+	Reg1x: reg1 PORT MAP(ref_clk=>ref_clk, DataI=>IR_out, en=>stallD, clr=>PCSrcD,
 					DataO=>InstrD);
 
+	controlx: control PORT MAP(Op=>InstrD(31 DOWNTO 26), Funct=>InstrD(31 DOWNTO 0),
+					RegWriteD=>RegWriteD, MemtoRegD=>MemtoRegD, MemWriteD=>MemWriteD,
+					ALUControlD=>ALUControlD, ALUSrcD=>ALUSrcD, RegDstD=>RegDstD,
+					BranchD=>BranchD);
 
+	regfilex: regfile PORT MAP(ref_clk=>ref_clk, we=>RegWriteW, raddr_1=>InstrD(25 DOWNTO 21),
+					raddr_2=>InstrD(20 DOWNTO 16), waddr=>WriteRegW, rdata_1=>RD1_out,
+					rdata_2=>RD2_out, wdata=>ResultW);
+
+	sign_extension_16bitx: sign_extension_16bit PORT MAP(immediate=>InstrD(15 DOWNTO 0),
+					sign_extension_out=>SignImmD);
+
+	shiftleft_32bitx: shiftleft_32bit PORT MAP(A_in=>SignImmD, O_out=>shift_out);
 
 
 end behavior;
