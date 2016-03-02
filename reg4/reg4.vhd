ebd70f522f210d8819e1ca433bcb328944f90236
Linda Vang <lvang2@uci.edu>
2016-03-02 11:34:24 -0800

    initializing variables in reg3 and reg4

--- reg4/reg4.vhd
+++ reg4/reg4.vhd
@@ -40,11 +40,11 @@ end reg4;
 architecture behavior of reg4 is
 begin
 	process(ref_clk, RegWriteM, MemtoRegM, rd_in, alu_in, WriteRegM)
-	variable temp_RegWriteM : 	std_logic;
-	variable temp_MemtoRegM : 	std_logic;
-	variable temp_rd_in:			std_logic_vector(31 DOWNTO 0);
-	variable temp_alu_in:			std_logic_vector(31 DOWNTO 0);
-	variable temp_WriteRegM:		std_logic_vector(4 DOWNTO 0);
+	variable temp_RegWriteM : 	std_logic := '0';
+	variable temp_MemtoRegM : 	std_logic := '0';
+	variable temp_rd_in:			std_logic_vector(31 DOWNTO 0) := "00000000000000000000000000000000";
+	variable temp_alu_in:			std_logic_vector(31 DOWNTO 0) := "00000000000000000000000000000000";
+	variable temp_WriteRegM:		std_logic_vector(4 DOWNTO 0) := "00000";
 	begin
 
 		if(ref_clk'event AND ref_clk='0') then 
